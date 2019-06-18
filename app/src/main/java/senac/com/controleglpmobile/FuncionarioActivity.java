38a0d254db131cd47c74b86bdc50448c9286cdb9
Leo <leo-m-abreu@live.com>
2019-06-18 11:49:20 -0300

    Commit com a activity do funcionario

---
+++ app/src/main/java/senac/com/controleglpmobile/FuncionarioActivity.java
@@ -0,0 +1,13 @@
+package senac.com.controleglpmobile;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FuncionarioActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_funcionario);
+    }
+}
