8393497af39d07d48bde60402c0ad4799523647f
rossi <rossievangelista@gmail.com>
2019-06-23 20:21:55 -0300

    feito interface login

---
+++ app/src/main/java/senac/com/controleglpmobile/view/CadClienteActivity.java
@@ -0,0 +1,15 @@
+package senac.com.controleglpmobile.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import senac.com.controleglpmobile.R;
+
+public class CadClienteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cad_cliente);
+    }
+}
---
+++ app/src/main/java/senac/com/controleglpmobile/view/ClienteActivity.java
@@ -0,0 +1,15 @@
+package senac.com.controleglpmobile.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import senac.com.controleglpmobile.R;
+
+public class ClienteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cliente);
+    }
+}
