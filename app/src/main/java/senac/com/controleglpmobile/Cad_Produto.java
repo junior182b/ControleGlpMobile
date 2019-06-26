e0550d436816f96f10231e7cb0e2e69f0da03a01
ControleGlp <pedro.neto@cursos.sc.senac.br>
2019-06-25 21:26:19 -0300

    Commit Produto e ProdutoDao by Pedro

---
+++ app/src/main/java/senac/com/controleglpmobile/Cad_Produto.java
@@ -0,0 +1,13 @@
+package senac.com.controleglpmobile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Cad_Produto extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cad_prod);
+    }
+}
